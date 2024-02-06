ab5413e3e2633ed652790ba248937b5d58eff304
Valentina Restrepo <vrestrepomontoya@gmail.com>
2024-02-05 22:32:02 -0500

    Codigo

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  deshabilitar = true;
+  titulo = 'Aplicación calculadora';
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+  sumar():void{
+    this.resultado = this.operandoA + this.operandoB;
+  }
+
+  restar():void{
+    this.resultado = this.operandoA - this.operandoB;
+  }
+
+  multiplicar():void{
+    this.resultado = this.operandoA * this.operandoB;
+  }
+}
